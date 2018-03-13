e1bec974af5750006fd6d9b36accd506893270bb
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-13 15:28:26 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,29 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  characters = character_hash["results"]
+  filmInfo = []
+    #When key == "name"
+  characters.each do |hash|#array
+
+      if hash["name"] == character
+
+          hash["films"].each do |url|
+            film = RestClient.get(url)
+            filmHash = JSON.parse(film)
+            filmInfo << filmHash
+          end
+
+        end
+  end
+  filmInfo
+    #Check to see if it's == to character
+    # if characters["name"] == character
+    #   characters["films"].each do |url|
+    #   binding.pry
+    # end
+  #
+  # end
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,7 +41,13 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts "***************************"
+    puts "Title: " + film["title"]
+    puts "Director: " + film["director"]
+    puts "Producer: " + film["producer"]
+    puts "Release Date: " + film["release_date"] 
+  end
 end
 
 def show_character_movies(character)
@@ -28,6 +56,6 @@ def show_character_movies(character)
 end
 
 ## BONUS
-
+get_character_movies_from_api("Luke Skywalker")
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  'Hello, Welcome to Star Wars Central'
 end
 
 def get_character_from_user
   puts "please enter a character"
+  name = gets.chomp#
   # use gets to capture the user's input. This method should return that input, downcased.
 end
